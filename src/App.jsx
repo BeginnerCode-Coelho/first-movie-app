273039d1b2edaa6b3140c49b7711b5ad2f59a419
Your Name <you@example.com>
2026-04-17 18:53:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { use, useEffect, useState } from 'react'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Search from './components/Search'
+import Spinner from './components/Spinner'
+import { MovieCard } from './components/MovieCard'
+import { getTrendingMovies, updateSearchCount } from './appwrite'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  }
+}
+
+
+function App() {
+ const [searchTerm, setSearchTerm] = useState('')
+
+ const [movieList, setMovieList] = useState([]);
+ const [errorMessage, setErrorMessage] = useState('');
+ const [isLoading, setIsLoading] = useState(false);
+
+ const [trendingMovies, setTrendingMovies] = useState([]);
+
+ const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+  try {
+
+    const endpoint = query
+     ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`
+     : `${API_BASE_URL}?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, API_OPTIONS);
+
+   if (!response.ok) { throw new Error('Failed to fetch movies');}
+    
+    const data = await response.json();
+    
+    
+     if(data.response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount({ searchTerm: query, movie: data.results[0] });
+      } ;
+
+  } catch (error) {
+    console.error(error);
+    setErrorMessage('Error fetching movies. Please try again later.');
+  } finally {
+    setIsLoading(false);
+  }
+ }
+ 
+ useEffect(() => {
+  const delayDebounceFn = setTimeout(() => {
+  fetchMovies(searchTerm);
+}, 500);
+return () => clearTimeout(delayDebounceFn);
+ }, [searchTerm]);
+
+ useEffect(() => {
+   const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  }
+  loadTrendingMovies();
+ }, []);
+ 
+
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+         <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+               <li key={movie.$id}>
+                <p>{index + 1}</p>
+                <img src={movie.poster_url} alt={movie.title} />
+               </li>
+              ))}
+            </ul>
+            </section>
+        )}
+        
+        <section className="all-movies">
+          <h2 >All Movies</h2>
+
+          {isLoading ? (
+           <Spinner/>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+           <ul>
+              {movieList.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+              ))}
+           </ul>
+            
+            
+          )}
+        </section>
+        
+      </div>
+    </main>
+  )
+}
+
+
+export default App
---
+++ src/appwrite.js
@@ -0,0 +1,49 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+
+const client = new Client()
+  .setEndpoint('https://fra.cloud.appwrite.io/v1') // Your Appwrite Endpoint
+  .setProject(PROJECT_ID); // Your project 
+  
+  const database = new Databases(client);
+
+export async function updateSearchCount({ searchTerm, movie}) {
+  // 1. Use Appwrite SDK to check if a search term exists in the database
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [ Query.equal('searchTerm', searchTerm) ]);
+
+    // 2. If it exists, update the search count
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, { count: doc.count + 1 });
+    } 
+    // 3. If it doesn't exist, create a new document with count = 1
+    else { 
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), { searchTerm, count: 1, movie_id: movie.id,
+      poster_url:`https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+       });
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+  
+}
+
+export async function getTrendingMovies() {
+  try {
+   const result = await database.listDocuments( DATABASE_ID, COLLECTION_ID, [ Query.limit(5),
+    Query.orderDesc('count')
+   ]);
+
+   return result.documents;
+
+  } catch (error) {
+    console.error(error);
+  } 
+  
+};
